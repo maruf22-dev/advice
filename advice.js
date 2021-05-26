91e238511884bb74e4c13cf8f73b470f2711254f
maruf22-dev <marufbinsalim22@gmail.com>
2021-05-27 03:15:00 +0600

    simple site that uses a simple API

---
+++ advice.js
@@ -0,0 +1,28 @@
+
+updateAdviceOnSite();
+async function updateAdviceOnSite()
+{
+    document.getElementById("mainText").innerHTML ="Loading . . ."
+    getNewAdvice();
+    
+}
+async function getNewAdvice()
+{
+    fetch('https://api.adviceslip.com/advice').
+    then(res => res.text()).then(data=> changeGlobalAdvice(data));
+}
+async function changeGlobalAdvice(res)
+{   console.log(res);
+    var globalAdvice = JSON.parse(res).slip.advice;
+        document.getElementById("mainText").innerHTML = '"' + globalAdvice + '"';
+        
+}
+
+ function buttonControl()
+{
+    document.getElementById("advBtn").disabled = true;
+    setTimeout( function() {
+        document.getElementById("advBtn").disabled = false;
+        updateAdviceOnSite();
+      }, 5000)
+}
